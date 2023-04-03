834dfea5aea599193c94ee7c363799ac4b9a60a7
Angel39706 <96604953+Angel39706@users.noreply.github.com>
2023-04-03 09:34:28 -0700

    UI Changes

--- ScreenInfo.py
+++ ScreenInfo.py
@@ -1,45 +1,66 @@
-import tkinter as tk
+from tkinter import *
+from getpass import getpass
+# Create object
+root = Tk()
+root.title('testing')
+# Adjust size
+root.geometry('800x480')
 
-#User interface begins here
-root = tk.Tk()
-
-canvas = tk.Canvas(root, width = 720, height = 576)
-canvas.grid(columnspan= 4,rowspan=4)
-
-#defining function for action taking placed when the button(admin) is tapped
-def adminWin():
-    window = tk()
-    canvas2 = tk.Canvas(root, width = 720, height = 576)
-    canvas2.grid(columnspan= 4,rowspan=4)
-    admin_text = tk.StringVar()
-    admin_button = tk.Button(root,textvariable= admin_text, font= "Bold", foreground= "red", command= adminWin)
-    admin_text.set("ADMIN")
-    admin_button.grid(column=2, row =0)
-    admin_button.config(height= 5, width=15) 
-
-
-#Admin Button
-admin_text = tk.StringVar()
-admin_button = tk.Button(root,textvariable= admin_text, font= "Bold", foreground= "red", command= adminWin)
-admin_text.set("ADMIN")
-admin_button.grid(column=2, row =0)
-admin_button.config(height= 5, width=15)
-
-
-#defining function for action taking placed when the button(scan) is tapped
-def ScanMess():
-    print("Scanning button Clicked!")
-
-#User Button
-scan_text = tk.StringVar()
-scan_button = tk.Button(root, textvariable= scan_text, font="Raleway", command= ScanMess)
-scan_text.set("Scan")
-scan_button.grid(column=0,row=1)
-scan_button.config(height= 10, width=40)
+def open():
+    top = Tk()
+    top.geometry('800x480')
+    top.title('Admin Settings')
+    pswrd_entry = Entry(top, width=25, show="*").grid(row=0,column = 1,sticky="NSEW" )
+    pswrd_tempt = Button(top,width= 25,height=2,text="Enter", command = passentry).grid(row=1,column = 1,sticky="NSEW")
+    cls_btn = Button(top,text="Home",width=5,height=2, command= top.destroy).grid(row=0,column = 0,sticky="NSEW")
+    numOne_btn =Button(top,text= "1",height=5,width=5,command= lambda: print("tapped 1")).grid(row=2,column = 2)
+    numTwo_btn =Button(top,text= "2",height=5,width=5,command= lambda:print("tapped 2")).grid(row=2,column = 3)
+    numThree_btn =Button(top,text= "3",height=5,width=5,command= lambda:print("tapped 3")).grid(row=2,column = 4)
+    numFour_btn =Button(top,text= "4",height=5,width=5,command= lambda:print("tapped 4")).grid(row=3,column = 2)
+    numFive_btn =Button(top,text= "5",height=5,width=5,command= lambda:print("tapped 5")).grid(row=3,column = 3)
+    numtSix_btn =Button(top,text= "6",height=5,width=5,command= lambda:print("tapped 6")).grid(row=3,column = 4)
+    numSeven_btn =Button(top,text= "7",height=5,width=5,command= lambda:print("tapped 7")).grid(row=4,column = 2)
+    numEight_btn =Button(top,text= "8",height=5,width=5,command= lambda:print("tapped 8")).grid(row=4,column = 3)
+    numNine_btn =Button(top,text= "9",height=5,width=5,command= lambda:print("tapped 9")).grid(row=4,column = 4)
+    numSeven_btn =Button(top,text= "*",height=5,width=5,command= lambda:print("tapped *")).grid(row=5,column = 2)
+    numEight_btn =Button(top,text= "0",height=5,width=5,command= lambda:print("tapped 0")).grid(row=5,column = 3)
+    numNine_btn =Button(top,text= "#",height=5,width=5,command= lambda:print("tapped #")).grid(row=5,column = 4)
 
 
+def scanning():
+    print("scan tapped")
 
+def passentry():
+    userMod= Toplevel()
+    userMod.geometry('800x480')
+    userMod.title('User Modification')
+    addUser_btn = Button(userMod,text="Add User",height=25,width=40, command=newUser).grid(row=1,column = 0,sticky="NSEW")
+    removeUser_btn = Button(userMod,text="Remove User",height=25,width=40, command= removeUser).grid(row=1,column = 1,sticky="NSEW")
 
+def newUser():
+    adduser= Toplevel()
+    adduser.geometry('800x480')
+    adduser.title('Add New User')
 
-#code after mainloop will not display
-root.mainloop()
+def removeUser():
+    removeuser= Toplevel()
+    removeuser.geometry('800x480')
+    removeuser.title('Remove Existing User')
+
+# Specify Grid
+Grid.rowconfigure(root,0,weight=1)
+Grid.columnconfigure(root,0,weight=1)
+Grid.rowconfigure(root,1,weight=1)
+Grid.columnconfigure(root,1,weight=1)
+Grid.rowconfigure(root,2,weight=1)
+ 
+# Create Buttons
+button_1 = Button(root,text="Scan",height=20, command= scanning)
+button_2 = Button(root,text="Admin", command= open)
+ 
+# Set grid
+button_1.grid(row=0,column=0,sticky="NSEW")
+button_2.grid(row=0,column=1,sticky="NSEW")
+ 
+# Execute tkinter
+mainloop()
--- testing.py
+++ testing.py
@@ -1,17 +1,66 @@
-from tkinter import*
+from tkinter import *
+from getpass import getpass
+# Create object
+root = Tk()
+root.title('testing')
+# Adjust size
+root.geometry('800x480')
 
-root =Tk()
-root.minsize(height = 500, width = 900)
-def tab1():
-    def tab2():
-        label1.destroy()
-        button1.destroy()
-        label2 = Label(root, text = "testing 2", font=('Times_New_Roman',25))
-        label2.pack()
-        button2 = Button(root, text = 'Back',font=('Times_New_Roman',25 ), command= tab1)
-        button2.pack(side = CENTER)
-    label1 = Label(root, text = "testing 1", font=('Times_New_Roman',25))
-    label1.pack()
-    button1 = Button(root, text = 'next',font=('Times_New_Roman',25 ), command =tab2)
-    button1.pack(side = CENTER)
-    root.mainloop()
+def open():
+    top = Tk()
+    top.geometry('800x480')
+    top.title('Admin Settings')
+    pswrd_entry = Entry(top, width=25, show="*").grid(row=0,column = 1,sticky="NSEW" )
+    pswrd_tempt = Button(top,width= 25,height=2,text="Enter", command = passentry).grid(row=1,column = 1,sticky="NSEW")
+    cls_btn = Button(top,text="Home",width=5,height=2, command= top.destroy).grid(row=0,column = 0,sticky="NSEW")
+    numOne_btn =Button(top,text= "1",height=5,width=5,command= lambda: print("tapped 1")).grid(row=2,column = 2)
+    numTwo_btn =Button(top,text= "2",height=5,width=5,command= lambda:print("tapped 2")).grid(row=2,column = 3)
+    numThree_btn =Button(top,text= "3",height=5,width=5,command= lambda:print("tapped 3")).grid(row=2,column = 4)
+    numFour_btn =Button(top,text= "4",height=5,width=5,command= lambda:print("tapped 4")).grid(row=3,column = 2)
+    numFive_btn =Button(top,text= "5",height=5,width=5,command= lambda:print("tapped 5")).grid(row=3,column = 3)
+    numtSix_btn =Button(top,text= "6",height=5,width=5,command= lambda:print("tapped 6")).grid(row=3,column = 4)
+    numSeven_btn =Button(top,text= "7",height=5,width=5,command= lambda:print("tapped 7")).grid(row=4,column = 2)
+    numEight_btn =Button(top,text= "8",height=5,width=5,command= lambda:print("tapped 8")).grid(row=4,column = 3)
+    numNine_btn =Button(top,text= "9",height=5,width=5,command= lambda:print("tapped 9")).grid(row=4,column = 4)
+    numSeven_btn =Button(top,text= "*",height=5,width=5,command= lambda:print("tapped *")).grid(row=5,column = 2)
+    numEight_btn =Button(top,text= "0",height=5,width=5,command= lambda:print("tapped 0")).grid(row=5,column = 3)
+    numNine_btn =Button(top,text= "#",height=5,width=5,command= lambda:print("tapped #")).grid(row=5,column = 4)
+
+
+def scanning():
+    print("scan tapped")
+
+def passentry():
+    userMod= Toplevel()
+    userMod.geometry('800x480')
+    userMod.title('User Modification')
+    addUser_btn = Button(userMod,text="Add User",height=25,width=40, command=newUser).grid(row=1,column = 0,sticky="NSEW")
+    removeUser_btn = Button(userMod,text="Remove User",height=25,width=40, command= removeUser).grid(row=1,column = 1,sticky="NSEW")
+
+def newUser():
+    adduser= Toplevel()
+    adduser.geometry('800x480')
+    adduser.title('Add New User')
+
+def removeUser():
+    removeuser= Toplevel()
+    removeuser.geometry('800x480')
+    removeuser.title('Remove Existing User')
+
+# Specify Grid
+Grid.rowconfigure(root,0,weight=1)
+Grid.columnconfigure(root,0,weight=1)
+Grid.rowconfigure(root,1,weight=1)
+Grid.columnconfigure(root,1,weight=1)
+Grid.rowconfigure(root,2,weight=1)
+ 
+# Create Buttons
+button_1 = Button(root,text="Scan",height=20, command= scanning)
+button_2 = Button(root,text="Admin", command= open)
+ 
+# Set grid
+button_1.grid(row=0,column=0,sticky="NSEW")
+button_2.grid(row=0,column=1,sticky="NSEW")
+ 
+# Execute tkinter
+mainloop()
